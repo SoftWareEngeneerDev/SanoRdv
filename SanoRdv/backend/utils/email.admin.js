45d2b9f8acebbcd2945adaf034ba0590ec585e8e
KAGAMBEGA Boukary <104064641+SoftWareEngeneerDev@users.noreply.github.com>
2025-07-14 11:18:44 +0000

    Correction des erreurs et l'exection de la base de données

--- SanoRdv/backend/utils/email.admin.js
+++ SanoRdv/backend/utils/email.admin.js
@@ -15,12 +15,9 @@ const createTransport = () => {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_PASS,
       },
-<<<<<<< HEAD
-=======
       tls: {
         rejectUnauthorized: false // Permet de désactiver les vérifications de certificat (utile pour le dev)
       },
->>>>>>> origin/master
     });
   }
 
@@ -33,12 +30,9 @@ const createTransport = () => {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       },
-<<<<<<< HEAD
-=======
       tls: {
         rejectUnauthorized: false // Permet de désactiver les vérifications de certificat (utile pour le dev)
       },
->>>>>>> origin/master
     });
   }
 
@@ -50,12 +44,9 @@ const createTransport = () => {
       user: 'ethereal.user@ethereal.email',
       pass: 'ethereal.pass',
     },
-<<<<<<< HEAD
-=======
     tls: {
       rejectUnauthorized: false // Permet de désactiver les vérifications de certificat (utile pour le dev)
     },
->>>>>>> origin/master
   });
 };
 
@@ -192,8 +183,4 @@ export const testEmailConfig = async () => {
   } catch (error) {
     return { success: false, message: `Configuration email invalide: ${error.message}` };
   }
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> origin/master
