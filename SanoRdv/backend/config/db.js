dadfee0e6d10aa6e64a731987525c49db3f17d94
ckonate <konateconstant@gmail.com>
2025-07-25 15:12:35 +0200

    update config

--- SanoRdv/backend/config/db.js
+++ SanoRdv/backend/config/db.js
@@ -1,12 +1,34 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+   
+
+dotenv.config();
+
+const {
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOSTNAME,
+  MONGO_PORT,
+  MONGO_DB
+} = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 500,
+  connectTimeoutMS: 10000,
+};
+const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/sanoRdv');
-    console.log('MongoDB connecté');
+    console.log('MONGO_URI:', url);
+    //await mongoose.connect(process.env.MONGO_URI);
+	await mongoose.connect(url);
+    console.log('✅ MongoDB connecté avec succès');
   } catch (error) {
-    console.error('Erreur de connexion à MongoDB', error);
-    process.exit(1); // arrêt du serveur si échec
+    console.error('❌ Échec de la connexion à MongoDB:', error);
+    process.exit(1);
   }
 };
 
